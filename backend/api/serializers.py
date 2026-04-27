3c04676def3e99d8a0da2f8d3954c7981df254d3
Abdulrahim201 <98453313+Abdulrahim201@users.noreply.github.com>
2026-04-28 02:22:08 +0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, Field, FieldUpdate
 
 
 class UserSerializer(serializers.ModelSerializer):
