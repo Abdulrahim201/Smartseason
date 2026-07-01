0b58f313a5d742c412786b435df7f0a1927e850e
Abdulrahimshaffy201 <abdulrahimshaffy201@gmail.com>
2026-07-01 12:03:06 +0300

    fix missing TemplateView import

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
